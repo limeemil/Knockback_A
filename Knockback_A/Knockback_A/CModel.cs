3027f3f3cab19a2dff142335f7893d116509c2d1
limeemil <limeemil@users.noreply.github.com>
2016-04-14 14:38:15 +0200

    Created a base project for bounding boxes.

---
+++ Knockback_A/Knockback_A/CModel.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knockback_A {
+    public class CModel {
+        public Vector3 Position { get; set; }
+        public Vector3 Rotation { get; set; }
+        public Vector3 Scale { get; set; }
+
+        public Model Model { get; private set; }
+        private Matrix[] modelTransforms;
+
+        private GraphicsDevice graphicsDevice;
+
+        public CModel(Model Model, Vector3 Position, Vector3 Rotation,
+            Vector3 Scale, GraphicsDevice graphicsDevice) {
+            this.Model = Model;
+
+            modelTransforms = new Matrix[Model.Bones.Count];
+            Model.CopyAbsoluteBoneTransformsTo(modelTransforms);
+
+            this.Position = Position;
+            this.Rotation = Rotation;
+            this.Scale = Scale;
+
+            this.graphicsDevice = graphicsDevice;
+        }
+
+        public void Draw(Matrix View, Matrix Projection) {
+            Matrix baseWorld = Matrix.CreateScale(Scale)
+                * Matrix.CreateFromYawPitchRoll(
+                    Rotation.Y, Rotation.X, Rotation.Z)
+                * Matrix.CreateTranslation(Position);
+
+            foreach (ModelMesh mesh in Model.Meshes) {
+                foreach (BasicEffect effect in mesh.Effects) {
+                    effect.EnableDefaultLighting();
+                    effect.World = modelTransforms[mesh.ParentBone.Index] *
+                        Matrix.CreateTranslation(Position);
+                    effect.View = View;
+                    effect.Projection = Projection;
+                    effect.EnableDefaultLighting();
+                }
+                mesh.Draw();
+            }
+        }
+    }
+}
---
+++ Knockback_A/Knockback_A/Game1.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace Knockback_A {
+    
+    public class Game1 : Game {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        List<CModel> models = new List<CModel>();
+
+        Vector3 modelPosition = Vector3.Zero;
+        Vector3 cameraPosition = new Vector3(0.0f, 0.0f, 50.0f);
+
+        Model chopper;
+        float aspectRatio;
+        Matrix[] modelTransforms;
+
+        public Game1() {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 800;
+        }
+
+        
+        protected override void Initialize() {
+
+            graphics.IsFullScreen = false;
+
+            base.Initialize();
+        }
+
+        
+        protected override void LoadContent() {
+            
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            aspectRatio = graphics.GraphicsDevice.Viewport.AspectRatio;
+
+            for (int y = 0; y < 3; y++)
+                for (int x = 0; x < 3; x++) {
+                    Vector3 position = new Vector3(
+                        -600 + x * 600, -400 + y * 400, 0);
+
+                    models.Add(new CModel(Content.Load<Model>("Chopper"), position,
+                        new Vector3(0, MathHelper.ToRadians(90) * (y * 3 + x), 0),
+                        new Vector3(0.25f), GraphicsDevice));
+                }
+        }
+
+        
+        protected override void UnloadContent() {
+            
+        }
+
+        
+        protected override void Update(GameTime gameTime) {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            
+
+            base.Update(gameTime);
+        }
+
+        
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            Matrix view = Matrix.CreateLookAt(cameraPosition, Vector3.Zero, Vector3.Up);
+
+            Matrix projection = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.ToRadians(45.0f), aspectRatio, 1.0f, 10000.0f);
+            
+
+
+            foreach (CModel model in models)
+                model.Draw(view, projection);
+            
+
+            base.Draw(gameTime);
+        }
+    }
+}
