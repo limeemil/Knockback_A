a97c9a0acdfd951d1691a46f5c843baf6810e109
limeemil <limeemil@users.noreply.github.com>
2016-04-18 21:33:13 +0200

    Addition of camera files.

---
+++ Knockback_A/Knockback_A/TargetCamera.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knockback_A
+{
+    public class TargetCamera : Camera
+    {
+        public Vector3 Position { get; set; }
+        public Vector3 Target { get; set; }
+
+        public TargetCamera(Vector3 Position, Vector3 Target,
+            GraphicsDevice graphicsDevice) : base(graphicsDevice)
+        {
+            this.Position = Position;
+            this.Target = Target;
+        }
+
+        public override void Update()
+        {
+            
+            this.View = Matrix.CreateLookAt(Position, Target, Vector3.Up);
+        }
+    }
+}
